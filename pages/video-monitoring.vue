258efddded512fd9bb18f7dad54927baffbdf8af
Florent Boutin <fboutin@skalv-studio.fr>
2021-08-13 12:44:58 +0200

    Can add manually new cam

--- pages/video-monitoring.vue
+++ pages/video-monitoring.vue
@@ -68,6 +68,45 @@
             src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QA/wD/AP+gvaeTAAABMElEQVRYhe2VT07CQBTGfxotC1NwCx7BhYfwAKg3YIvAmTiLRNEEGxcW9BqicYmLeU0mhc6ftuNqvuSlad/3vvfNtG8KERER7jgDcmBniVy4rWPu0LyIed0mNuFf4MpQfwn8OOjUNjByWMSoDQOhsKd/bCl4BBalZx1gCjwD3xJLYAIkbTtcAA/a/QXwRvXWZsDAQ9+L0NGa58AQSCVugI3kXqneiUYGZpJbA70D+Z5m4j6EgRfJDQ31t8JZhjCwlVxqqO8K58tV3zYF5WIbjjz0vA2s5Xpt4BS5d18jBUyvYCK5DYc/wnPgUzjjGvpWQoKa8x3wgfrguhJ3WvMVcBrCAKhDJtN45VgB/Qb6Tv+CBDXnT6jJ2KKO7DHVK2/VQBPs6Z8YiP8CnzGMiIgIgj+zDqDGr02gKAAAAABJRU5ErkJggg=="
           />
         </div>
+        <div class="videoPreview" @click="openManualAddCamModal">
+          <p>Ajout manuel</p>
+          <img
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QA/wD/AP+gvaeTAAACAElEQVRYhe3WvU4UURQH8B8m7BZAoVEbSzs2BhtFH8EaPworHmB5E11cXoA3kBo6iYlrLEyQCiuWJxAMS1yzFvdOdrLMzM4MS2HCP7mZO/d8/e+Ze84dbnCD/wgLOMRoyjiMujPHdongydiuG2Sa43OsFNi38LuEn9oE1ktsYn0WBK4Ll/zfmmLwGfsTa01s4AvOhJT30EZj1gz38Sn1/gDf5af2G+5U8F9JoZkKfow1LOEu3uAoynryM3ElAhup4Fm7vI+fUad9HQR6UbZWYP8SO3iRI/+Iv3UJnEbZUpGDEnhYl0DSYO5NCdBCBz+EKjmL806UFaLMJ3idI29gC0P5VTJEV0G5FhFoR9mRcOAmg+8Zt+tNPBUupQWs4gMGUWcvj0QRgYZQ5yPhtL9KybaMK+RRjj3hHulH3W5VAnAbX6POTlxrCak9zwie5W9FyMQQy1UJEDLRNi61TrTpZOjm+evG9fd1CEwi+Ul5UoHAalw/yDMoM5rRJukPiyV9iLoj/Jp2GxZhLhWwKpK4o0kCcxXGINr043M5w0eW37Tu8VUykGA3Pt9WsEl0dwu1SiIpw4HL/4tZh/AxLuSUYV0kZdXPIDEZ/CTqbs4qOMwbt+JBJPRMOO2LeC50ywvjVjw/SwKEBtVVfBn9EXY+8+BptIQOdyD0iNM4fyfjm/8D7vX0jqAsH8QAAAAASUVORK5CYII="
+          />
+        </div>
+
+        <b-modal
+          id="manualAddCamModal"
+          title="Nouvelle camera"
+          @show="resetModal"
+          @hidden="resetModal"
+          @ok="handleOk"
+        >
+          <form ref="form" @submit.stop.prevent="addManualNewCam">
+            <b-form-group
+              label="Nom de la camera"
+              label-for="manualNewCamName"
+              invalid-feedback="Name is required"
+            >
+              <b-form-input
+                id="manualNewCamName"
+                v-model="manualNewCam.name"
+                required
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group
+              label="Ip de la camera"
+              label-for="manualNewCamIp"
+              invalid-feedback="Ip is required"
+            >
+              <b-form-input
+                id="manualNewCamIp"
+                v-model="manualNewCam.ip"
+                required
+              ></b-form-input>
+            </b-form-group>
+          </form>
+        </b-modal>
       </div>
     </div>
   </div>
@@ -79,7 +118,8 @@ export default {
     return {
       videoList: [],
       currentVideo: null,
-      connectToFernand: false
+      connectToFernand: false,
+      manualNewCam: {}
     }
   },
   mounted() {
@@ -125,6 +165,28 @@ export default {
       this.$axios.$get(
         `http://${this.currentCam.ip}/control?var=rotate_x&val=${this.pos_x}`
       )
+    },
+    openManualAddCamModal() {
+      this.$bvModal.show("manualAddCamModal")
+    },
+    resetModal() {
+      this.manualNewCam = {}
+    },
+    handleOk(bvModalEvt) {
+      // Prevent modal from closing
+      bvModalEvt.preventDefault()
+      // Trigger submit handler
+      this.addManualNewCam()
+    },
+    addManualNewCam() {
+      console.log(this.manualNewCam)
+      this.videoList.push({
+        name: this.manualNewCam.name,
+        ip: this.manualNewCam.ip
+      })
+      this.$nextTick(() => {
+        this.$bvModal.hide("manualAddCamModal")
+      })
     }
   }
 }
